27bc76d64708f80a083d38317cd4ac8e827e0cc5
Walid Cawasa <32444138+koribot@users.noreply.github.com>
2023-12-02 21:54:01 +0800

    Revert "✨ Feature home page (not done)"

--- store/common/store.ts
+++
@@ -1,5 +0,0 @@
-import { store } from "./actions";
-import { createContext, useContext } from "react";
-
-export const StoreContext = createContext(store);
-export const useStore = () => useContext(StoreContext);
