30d534f581d2b5785f035b29f70931f3bd677e43
Walid <w.cawasa@gmail.com>
2023-12-10 16:34:54 +0800

    :book: added baseURL checker using env

--- services/scrape_service.ts
+++ services/scrape_service.ts
@@ -6,12 +6,13 @@ const nextjs_endpoint_keyword = "/api/scrape/v1/keyword";
 const nextjs_endpoint_html = "/api/scrape/v1/html";
 const ENDPOINT_KEYWORD = 'endpoint_keyword';
 
+const baseURL = process.env.SITE_STATUS === "production" ? 'https://test-pi-three-54.vercel.app' : 'http://localhost:3000';
 const ScrapeService = {
  scrape: async (query: string, endpoint: string) => {
   const nextjsApiRoute =
    endpoint === ENDPOINT_KEYWORD ? nextjs_endpoint_keyword : nextjs_endpoint_html;
   try {
-   const response = await axios.get(`http://localhost:3000${nextjsApiRoute}?q=${query}`);
+   const response = await axios.get(`${nextjsApiRoute}?q=${query}`);
    return response;
   } catch (error) {
    console.error('Error:', error);
