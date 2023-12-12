58e9835493d49fde6077831d4c4425ed89a1f22c
Walid <w.cawasa@gmail.com>
2023-12-12 17:19:39 +0800

    :wrench: Centered ListingLists

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,17 @@
 import Image from 'next/image'
 import HomePage from '../components/home/HomePage'
 import { observer } from 'mobx-react-lite';
+import Navbar from '@/components/navbar/Navbar';
+import HomePageBody from '@/components/home/HomePageBody';
+import Footer from '@/components/footer/Footer';
 
 
 function Home() {
   return (
     <>
-      <HomePage />
+      <Navbar />
+      <HomePageBody />
+      <Footer />
     </>
   )
 }
--- components/listing/ListingLists.tsx
+++ components/listing/ListingLists.tsx
@@ -17,24 +17,26 @@ const ListingLists = ({ Listings, isLoading }: ListingsProps) => {
    title: '#',
    key: 'Title', // Provide a unique key for the column
    dataIndex: 'index',
-   width: '200px',
+   width: '50px',
    sorter: (a: any, b: any) => a.index - b.index,
    render: (index: number) => {
     return index + 1
-   }
+   },
+   fixed: 'top'
   },
   {
    title: 'Item Title',
    key: 'Title', // Provide a unique key for the column
    dataIndex: 'title',
-   width: '200px',
-   sorter: (a: any, b: any) => a.title.length - b.title.length
+   width: '500px',
+   sorter: (a: any, b: any) => a.title.length - b.title.length,
   },
   {
    title: 'Price',
    key: 'Price',
    dataIndex: 'price',
-   sorter: (a: any, b: any) => a.price - b.price
+   sorter: (a: any, b: any) => a.price - b.price,
+   width: '50px',
   },
   {
    title: 'Image',
@@ -47,7 +49,8 @@ const ListingLists = ({ Listings, isLoading }: ListingsProps) => {
      width={150}
      height={150}
     />
-   }
+   },
+   width: '200px'
   },
   {
    title: 'Item Link',
@@ -63,6 +66,7 @@ const ListingLists = ({ Listings, isLoading }: ListingsProps) => {
  return (
   <>
    <Table
+    sticky={{ offsetHeader: 0 }}
     dataSource={listingsWithKeys}
     columns={columns}
     loading={isLoading}
